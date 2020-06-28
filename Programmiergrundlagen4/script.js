95678ae4b2711ba79b39677770e5a14a043cf585
aylanemr <emre.aylan@hs-furtwangen.de>
2020-06-28 17:35:02 +0200

    #09

---
+++ Programmiergrundlagen4/script.js
@@ -0,0 +1,59 @@
+"use strict";
+// Inspieriert von w3schools
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    };
+}
+// Add a "erledigt" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('erledigt');
+    }
+}, false);
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("aufgaben").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    }
+    else {
+        document.getElementById("aufzaehlung").appendChild(li);
+    }
+    document.getElementById("aufgaben").value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        };
+    }
+}
+document.querySelector(".addBtn").addEventListener("click", newElement);
+setInterval(function () {
+    document.querySelector("#zahl").innerHTML = document.getElementsByTagName("li").length;
+});
+1000;
+//# sourceMappingURL=script.js.map
